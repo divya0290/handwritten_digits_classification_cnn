bd2782747b7454207a7aba640a480b05fc6c52bb
divya0290 <divyapal065@gmail.com>
2024-12-29 15:50:12 +0530

    added template/index.html and update app.py accordingly

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-
 app = Flask(__name__)
-
-
 model = tf.keras.models.load_model('mnist_cnn_model.h5')
 
 @app.route('/')
 def home():
-    return "Handwritten Digit Classifier API"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -23,13 +20,11 @@ def predict():
         return jsonify({'error': 'No selected file'}), 400
 
     try:
-       
         img = Image.open(file.stream).convert('L')  # Convert to grayscale
         img = img.resize((28, 28))                 # Resize to 28x28
-        img = np.array(img) / 255               # Normalize pixel values
+        img = np.array(img) / 255.0                # Normalize pixel values
         img = img.reshape(1, 28, 28, 1)            # Reshape to match model input
 
-       
         prediction = model.predict(img)
         digit = np.argmax(prediction)
 
